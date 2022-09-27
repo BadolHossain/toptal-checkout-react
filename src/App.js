bb34ee1647f1be6f94b915fdaa031120b39dfb31
BadolHossain <badolhossain01909@gmail.com>
2022-09-27 22:57:01 +0600

    done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Counter from "./components/counter";
 function App() {
   return (
     <div className="App">
-      <Checkout />
+      {/* <Checkout /> */}
       
-      {/* <Counter /> */}
+      <Counter />
     </div>
   );
 }
--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>zero</h1>
+      <h1>{count <= 0 ? "Zero" : count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)} disabled={count == 0}>
+        Decrement
+      </button>
     </div>
   );
 };
